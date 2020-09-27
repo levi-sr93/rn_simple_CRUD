56e93471e241168969c8784870e828ef51157ea9
Levi Santos <levi-sr@outlook.com>
2020-09-27 16:17:37 -0300

    Creating users MOCK

---
+++ src/mock_data/users.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'João Santos',
+    email: 'joaosan@email.com',
+    avatarURL:
+      'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png',
+  },
+  {
+    id: 2,
+    name: 'Mariana Aguiar',
+    email: 'riasmas@email.com',
+    avatarURL:
+      'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png',
+  },
+  {
+    id: 3,
+    name: 'Carlos José',
+    email: 'carljo@email.com',
+    avatarURL:
+      'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png',
+  },
+  {
+    id: 4,
+    name: 'Andressa Silva',
+    email: 'silvandr@email.com',
+    avatarURL:
+      'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027367_960_720.png',
+  },
+  {
+    id: 5,
+    name: 'Pedro Carvalho',
+    email: 'carvalhopedron@email.com',
+    avatarURL:
+      'https://cdn.pixabay.com/photo/2018/05/19/22/03/man-3414477_960_720.png',
+  },
+];
