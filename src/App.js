79755073b42c960e92e2ba488408a034d75a80c8
Levi Santos <levi-sr@outlook.com>
2020-09-27 16:08:51 -0300

    Navigation with Stack #2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import {Button, Icon} from 'react-native-elements';
+
 import UserList from './views/UserList';
 import UserForm from './views/UserForm';
 
@@ -11,10 +13,43 @@ const Stack = createStackNavigator();
 export default (props) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="UserList">
-        <Stack.Screen name="UserList" component={UserList} />
-        <Stack.Screen name="UserForm" component={UserForm} />
+      <Stack.Navigator
+        initialRouteName="UserList"
+        screenOptions={screenOptions}>
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={({navigation}) => {
+            return {
+              title: 'User List',
+              headerRight: () => (
+                <Button
+                  onPress={() => navigation.navigate('UserForm')}
+                  type="solid"
+                  icon={<Icon name="add" size={25} color="#fff" />}
+                />
+              ),
+            };
+          }}
+        />
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{
+            title: 'User Form',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
+};
+
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#1abc9c',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
 };
