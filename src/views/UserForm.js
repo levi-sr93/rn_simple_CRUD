5470dcc9d076c77ceb8f6d7d12fa288329fecac5
Levi Santos <levi-sr@outlook.com>
2020-09-29 20:05:30 -0300

    Rotas e Parametros

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text} from 'react-native';
 
-export default (props) => {
-  return <Text>UserForm</Text>;
+export default ({route, navigation}) => {
+  // console.warn(Object.keys(props.route.params));
+
+  const [user, setUser] = useState(route.params ? route.params : {});
+  // console.warn(user);
+
+  return <Text>{user.id}</Text>;
 };
