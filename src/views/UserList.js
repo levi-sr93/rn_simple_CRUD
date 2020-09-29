9f7362ef8fd4d6f0e72fd4a02054bce0c5facb2e
Levi Santos <levi-sr@outlook.com>
2020-09-29 19:13:27 -0300

    Listando usuários #1

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
+import {ListItem} from 'react-native-elements';
+
+import users from '../mock_data/users';
 
 export default (props) => {
-  return <Text>UserList</Text>;
+  // console.warn(Object.keys(props)); - Verficando as chaves que vem no objeto de props.
+  function getUserItem({item: user}) {
+    return (
+      <ListItem
+        leftAvatar={{source: {uri: user.avatarURL}}}
+        key={user.id}
+        title={user.name}
+        subtitle={user.email}
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm')}
+      />
+    );
+  }
+  return (
+    <View>
+      <FlatList
+        keyExtractor={(user) => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
+  );
 };
