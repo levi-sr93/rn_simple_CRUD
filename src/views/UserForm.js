9662b976ae94dfb6c3fc7251627f4d9c60e970d9
Levi Santos <levi-sr@outlook.com>
2020-09-29 21:54:49 -0300

    User form creation

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,11 +1,73 @@
 import React, {useState} from 'react';
-import {Text} from 'react-native';
+import {Text, TextInput, View, StyleSheet, Button} from 'react-native';
 
+//recebe os parametros a partir das rotas
 export default ({route, navigation}) => {
   // console.warn(Object.keys(props.route.params));
 
   const [user, setUser] = useState(route.params ? route.params : {});
   // console.warn(user);
 
-  return <Text>{user.id}</Text>;
+  return (
+    <View style={style.form}>
+      <Text>Name</Text>
+
+      <TextInput
+        style={style.input}
+        onChangeText={(name) =>
+          setUser({
+            ...user,
+            name,
+          })
+        }
+        placeholder="Inform name"
+        value={user.name}
+      />
+
+      <Text>E-mail</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={(email) =>
+          setUser({
+            ...user,
+            email,
+          })
+        }
+        placeholder="Inform email"
+        value={user.email}
+      />
+
+      <Text>Avatar URL</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={(avatarURL) =>
+          setUser({
+            ...user,
+            avatarURL,
+          })
+        }
+        placeholder="Avatar URL"
+        value={user.avatarURL}
+      />
+      <Button
+        title="Save"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
 };
+
+const style = StyleSheet.create({
+  input: {
+    height: 40,
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+
+  form: {
+    padding: 12,
+  },
+});
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -16,7 +16,7 @@ export default (props) => {
         },
       },
       {
-        text: 'No',
+        text: 'Cancel',
       },
     ]);
   }
